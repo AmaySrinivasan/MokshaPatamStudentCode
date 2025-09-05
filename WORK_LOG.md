a19526020f6eae3285dc11d4eb81b18b1c7dd36b
amaysrinivasan <amay.srinivasan@menloschool.org>
2025-09-04 22:05:34 -0700

    setup

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date   |   Time   |                                                                              Update |
+|:-------|:--------:|------------------------------------------------------------------------------------:|
+| Sept 3 | 2:30-3pm |                In class I tried to plan out my whole idea/approach to this question |
+| Sept 4 |  9-10pm  | Became a bit confused with BFS implementation, so just started with the basic setup |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -4,7 +4,7 @@
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Amay Srinivasan
  *
  */
 
@@ -15,6 +15,13 @@ public class MokshaPatam {
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
+        // I'm thinking either move 6 or take a ladder, and then branch out from there and test each of the options
+        // One thing to remember is that if they meet up on a square, say square 30, and one branch is move 5 and one is move 6
+        // You can already eliminate one of them.
+        Queue<Integer> positions = new LinkedList<>();
+        Queue<Integer> moves = new LinkedList<>();
+        // Use the bfs stuff based on what we learned earlier. just need to work towards more efficiency.
+        // Deleted a lot of bad code
 
         return 0;
     }
