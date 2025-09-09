bd5c6c3992a32c15ef5f0759845a4245d141724d
amaysrinivasan <amay.srinivasan@menloschool.org>
2025-09-08 22:24:46 -0700

    Finished up a version that actually compiles/works.

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -7,7 +7,7 @@
  * Completed by: Amay Srinivasan
  *
  */
-
+import java.util.LinkedList;
 public class MokshaPatam {
 
     /**
@@ -18,11 +18,46 @@ public class MokshaPatam {
         // I'm thinking either move 6 or take a ladder, and then branch out from there and test each of the options
         // One thing to remember is that if they meet up on a square, say square 30, and one branch is move 5 and one is move 6
         // You can already eliminate one of them.
-        Queue<Integer> positions = new LinkedList<>();
-        Queue<Integer> moves = new LinkedList<>();
-        // Use the bfs stuff based on what we learned earlier. just need to work towards more efficiency.
-        // Deleted a lot of bad code
+        int[] teleport = new int[boardsize + 1];
+        for (int i = 0; i<= boardsize; i++) {
+            teleport[i] = i;
+        }
+        for (int i = 0; i < ladders.length; i++) {
+            int from = ladders[i][0];
+            int to = ladders[i][1];
+            teleport[from] = to;
+        }
+        for (int i = 0; i < snakes.length; i++) {
+            int from = snakes[i][0];
+            int to = snakes[i][1];
+            teleport[from] = to;
+        }
+        boolean[]visited = new boolean[boardsize + 1];
+        visited[1] = true;
+        LinkedList<int[]> queue = new LinkedList <>();
+        queue.add(new int[]{1,0});
+        while (!queue.isEmpty()) {
+            int[] current = queue.remove(0);
+            int position = current[0];
+            int moves = current[1];
+            if (position == boardsize) {
+                return moves;
+            }
+            for (int diceRoll = 1; diceRoll < 7; diceRoll++) {
+                int nextSpot = position + diceRoll;
+                if (nextSpot <= boardsize) {
+                    int finalSquare = teleport[nextSpot];
+                    if(!visited[finalSquare]) {
+                        visited[finalSquare] = true;
+                        queue.add(new int[]{finalSquare, moves + 1});
+                    }
+                }
+            }
+        }
 
-        return 0;
+
+
+
+        return -1;
     }
 }
